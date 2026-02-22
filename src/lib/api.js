51a523d4fc8008dd66f389cae66d2ef2b1fb4de9
Vedanshu Nishad <vedanshun05@mail.com>
2026-02-22 07:44:10 +0530

    env change

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
 export async function submitVoice(audioBlob) {
     const formData = new FormData();
